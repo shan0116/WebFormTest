caacc88bcd2e94383c02d27e2bbd75d1c93e08ba
DESKTOP-9R6B0J5\DELL <heshan0116@@gmail.com>
2022-01-20 11:41:57 +0100

    Authentication + httpmodule + httphandler

---
+++ WebFormTest/WebFormTest/Handler/AppShutDownHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+
+namespace WebFormTest.Handler
+{
+    public class AppShutDownHandler : IHttpHandler
+    {
+        public bool IsReusable => true;
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("<h1 style='Color:blue; font-size:15px;'>Our website is under maintainace. Please try after some time</h1>");
+        }
+    }
+}
